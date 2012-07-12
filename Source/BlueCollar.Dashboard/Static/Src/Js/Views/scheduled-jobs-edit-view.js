1b082478c8adb6e1b456cf7e55107f7c17266e2d
Chad Burggraf <chad.burggraf@gmail.com>
2012-07-12 16:22:36 -0700

    Start implementing scheduled job CRUD. IMPORTANT:
      * Fetches are now happening when selecting or canceling out of
        ID views due to some regression. Fix this!

---
+++ Source/BlueCollar.Dashboard/Static/Src/Js/Views/scheduled-jobs-edit-view.js
@@ -0,0 +1,23 @@
+﻿/**
+ * Implements the scheduled jobs edit form.
+ *
+ * @constructor
+ * @extends {FormView}
+ */
+var ScheduledJobsEditView = FormView.extend({
+    serializers: {
+        "Id": new IntFieldSerializer()
+    },
+    template: _.template($('#scheduled-jobs-edit-template').html()),
+    validators: {
+
+    },
+
+    /**
+     * Focuses the first element in the form.
+     */
+    focus: function() {
+        this.$('input[name="JobType"]').focus();
+        return this;
+    }
+});
--- Source/BlueCollar.Dashboard/Static/Src/Js/Views/scheduled-jobs-view.js
+++ Source/BlueCollar.Dashboard/Static/Src/Js/Views/scheduled-jobs-view.js
@@ -21,9 +21,9 @@ var ScheduledJobsView = AreaView.extend({
 
     /**
      * Handle's the add button's click event.
-     *
+     */
     add: function() {
-        var model = new ScheduleModel({StartOn: Date.today()});
+        var model = new ScheduledJobModel();
         model.urlRoot = this.model.get('UrlRoot');
         this.model.clearId();
         this.renderIdView($('.details'), model);
@@ -34,15 +34,15 @@ var ScheduledJobsView = AreaView.extend({
      *
      * @param {jQuery} el The jQuery object containing the details element to render into.
      * @param {CollarModel} model The model to render the ID view for.
-     *
+     */
     renderIdView: function(el, model) {
-        var view = new SchedulesEditView({model: model});
+        var view = new ScheduledJobsEditView({model: model});
         view.bind('cancel', this.editCancel, this);
         view.bind('delete', this.editDelete, this);
         view.bind('submit', this.editSubmit, this);
         el.html(view.render().el);
         view.focus();
-    }*/
+    },
 
     /**
      * Renders the current schedule's name in its container.
