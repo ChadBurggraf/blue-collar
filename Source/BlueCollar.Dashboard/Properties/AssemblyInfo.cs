9ed647e54b6dfba64d605acf776736e2f74d1775
Chad Burggraf <chad.burggraf@gmail.com>
2012-07-23 09:38:37 -0700

    Clean up code analysis suppressions.

--- Source/BlueCollar.Dashboard/Properties/AssemblyInfo.cs
+++ Source/BlueCollar.Dashboard/Properties/AssemblyInfo.cs
@@ -12,9 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Blue Collar Dashboard")]
 [assembly: AssemblyDescription("Blue Collar web dashboard.")]
 [assembly: Guid("f6b60f5f-9f15-41e1-a370-08b36b98ab65")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "BlueCollar.Dashboard")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "BlueCollar.Dashboard.Controllers")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "BlueCollar.Dashboard.Models")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("BlueCollar.Test")]
--- Source/BlueCollar.SQLiteRepository/Properties/AssemblyInfo.cs
+++ Source/BlueCollar.SQLiteRepository/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Blue Collar SQLite repository plugin.")]
 [assembly: Guid("220d4a3a-c8db-4a0b-9db1-7a7a2b20c39e")]
 
-[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Lite")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "BlueCollar")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Lite", Justification = "The spelling is intentional.")]
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "BlueCollar", Justification = "The full namespace is part of the BlueCollar.dll assembly.")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("BlueCollar.Test")]
